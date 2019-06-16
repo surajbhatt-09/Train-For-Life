c73d37250ce314d2f07356f6c955321fa27f411e
surajbhatt-09 <srjbhatt376@gmail.com>
2019-06-16 13:04:58 +0530

    Updates

---
+++ precache-manifest.e34ebfed399645b321ab9b0fcffdf134.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350538b731f2a1d2fae633b73dfa3567",
+    "url": "/loveGym/index.html"
+  },
+  {
+    "revision": "1304c71ccefb2a70ab7f",
+    "url": "/loveGym/static/css/main.d225b8f8.chunk.css"
+  },
+  {
+    "revision": "52c3c85ea48af708a45e",
+    "url": "/loveGym/static/js/2.6f12d623.chunk.js"
+  },
+  {
+    "revision": "1304c71ccefb2a70ab7f",
+    "url": "/loveGym/static/js/main.209a9208.chunk.js"
+  },
+  {
+    "revision": "9e8b7662ed44e7705558",
+    "url": "/loveGym/static/js/runtime~main.e72ffed9.js"
+  },
+  {
+    "revision": "d4f0d9d7622e9f4a0dff45e3f22f1c8a",
+    "url": "/loveGym/static/media/diet.d4f0d9d7.jpg"
+  },
+  {
+    "revision": "1a5e66cb26e947470753880133337f68",
+    "url": "/loveGym/static/media/life.1a5e66cb.jpg"
+  },
+  {
+    "revision": "6c813eeebd7e0925251a35ce517189d5",
+    "url": "/loveGym/static/media/medi.6c813eee.jpg"
+  },
+  {
+    "revision": "9e2ff01b76dcf6968ab094206e9bae95",
+    "url": "/loveGym/static/media/work.9e2ff01b.jpg"
+  }
+]);
